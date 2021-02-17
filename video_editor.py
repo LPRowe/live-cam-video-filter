b5c61f5c29d2354e7d4936511e31351b6e354146
LPRowe <48843359+LPRowe@users.noreply.github.com>
2021-02-17 02:46:54 -0600

    added face eye and smile recognition currently debuggging offset from using face region as eye input

--- video_editor.py
+++ video_editor.py
@@ -1,3 +1,44 @@
+"""
+  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+
+  By downloading, copying, installing or using the software you agree to this license.
+  If you do not agree to this license, do not download, install,
+  copy or use the software.
+
+
+                        Intel License Agreement
+                For Open Source Computer Vision Library
+
+ Copyright (C) 2000, Intel Corporation, all rights reserved.
+ Third party copyrights are property of their respective owners.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+   * Redistribution's of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+   * Redistribution's in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+   * The name of Intel Corporation may not be used to endorse or promote products
+     derived from this software without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors "as is" and
+ any express or implied warranties, including, but not limited to, the implied
+ warranties of merchantability and fitness for a particular purpose are disclaimed.
+ In no event shall the Intel Corporation or contributors be liable for any direct,
+ indirect, incidental, special, exemplary, or consequential damages
+ (including, but not limited to, procurement of substitute goods or services;
+ loss of use, data, or profits; or business interruption) however caused
+ and on any theory of liability, whether in contract, strict liability,
+ or tort (including negligence or otherwise) arising in any way out of
+ the use of this software, even if advised of the possibility of such damage.
+"""
+
+
+
 # matrix filter: sliding repeat image of text that whose intensity is modified by a canny filter
 # AI line filter: ai lines for 5 by 5 squares or 7 by 7 squares
 #                 25 inputs one for each pixel 
@@ -23,10 +64,9 @@
 # Add a mask that can go over the video
 # Add a skew option
 # Add a drawing option
-# add a reset button
 # update readme (remember ctrl c and ctrl s functions work)
-
-
+#        arrow keys for mode and settings
+#        r key for reset
 
 import time
 import itertools
@@ -48,22 +88,30 @@ class Video:
         
         self.SUBMODES = {"TRANSLATE_X": 0,    # pixel offset x direction
                          "TRANSLATE_Y": 0,    # pixel offset y direction
-                         "FLIP": None,        # image flip horiz, vert, both
+                         "FLIP": 1,           # image flip horiz, vert, both
                          "ROTATE": 0,         # image rotation angle
                          "EDGE": None,        # change the edge detection mode
                          "SCALE_X": None,     # adjust x scale of the image
                          "SCALE_Y": None,     # adjust y scale of the image
                          "BLUR": None,        # control how much the image is blurred
-                         "COLOR": None        # adjust the image colors
+                         "COLOR": None,       # adjust the image colors
+                         "SCALE": 1,          # adjust the scale of the image
+                         "FACE": -1,          # adjust minimum neighbors for face detection
+                         "EYES": -1,          # adjust minimum neighbors for eyes detection
+                         "SMILE": -1          # adjust minimum neighbors for smile detection
                          }
         
         self.MESSAGE_TIME = time.time() + self.MESSAGE_DISPLAY_TIME * self.WELCOME_MESSAGE  # if time < MESSAGE_TIME then blit messages
-        
         self.MODE_WHEEL = ["FLIP", "EDGE", "BLUR", "COLOR", 
-                           "TRANSLATE_X", "TRANSLATE_Y", "ROTATE"] # list of modes
+                           "TRANSLATE_X", "TRANSLATE_Y", "ROTATE", 
+                           "SCALE", "FACE", "SMILE", "EYES"] # list of modes
         self.FLIP_WHEEL = itertools.cycle([None, 0, 1, -1]) # cycle through 3 image flip options
-        self.EDGE_WHEEL = itertools.cycle([None, self._edge_canny, self._edge_laplacian1, self._edge_laplacian2]) # cycle through 3 edge detectors
-        self.COLOR_WHEEL = itertools.cycle([None, self._hue_saturation_value, self._gray, self._lab, self._red, self._green, self._blue])
+        self.EDGE_INDEX = 0
+        self.EDGE_WHEEL = [None, self._edge_canny, self._edge_laplacian1, self._edge_laplacian2,
+                           self._sobel_x, self._sobel_y, self._sobel_xy] # cycle through edge detectors
+        
+        self.COLOR_INDEX = 0
+        self.COLOR_WHEEL = [None, self._hue_saturation_value, self._gray, self._lab, self._red, self._green, self._blue]
         
         # =============================================================================
         # USER MESSAGES BASED ON CURRENT ACTIVITY
@@ -77,7 +125,10 @@ class Video:
         self.EDGE_MESSAGES = {None: "No Corrections",
                               self._edge_canny: "Canny",
                               self._edge_laplacian1: "Laplacian 1",
-                              self._edge_laplacian2: "Laplacian 2"
+                              self._edge_laplacian2: "Laplacian 2",
+                              self._sobel_x: "Sobel X Gradient",
+                              self._sobel_y: "Sobel Y Gradient",
+                              self._sobel_xy: "Sobel"
                               }
         
         self.COLOR_MESSAGES = {None: "No Corrections",
@@ -92,30 +143,50 @@ class Video:
         self.BLUR_MESSAGES = lambda k: f"Kernel {k}" if k else "No Smoothing"
         self.ROTATE_MESSAGES = lambda alpha: f"Rotated {alpha} deg." if alpha else "No Rotation"
         self.TRANSLATE_MESSAGES = lambda dx, dy: f"(x, y): ({dx}, {dy})" if dx | dy else "No Translation"
+        self.SCALE_MESSAGES = lambda scale: f"Scale: {scale: .1f}" if scale != 1 else "Scale: Original Scale"
+        self.FACE_MESSAGES = lambda neigh: f"Min. Neighbors: {neigh}" if neigh != -1 else "Face Detection: OFF"
+        self.EYES_MESSAGES = lambda neigh: f"Eye Detection: ON" if neigh != -1 else "Eye Detection: OFF"
+        self.SMILE_MESSAGES = lambda neigh: f"Smile Detection: ON" if neigh != -1 else "Smile Detection: OFF"
         
     def reset(self):
-        self.SUBMODES = {"TRANSLATE_X": None, # pixel offset x direction
-                         "TRANSLATE_Y": None, # pixel offset y direction
-                         "FLIP": None,        # image flip horiz, vert, both
-                         "ROTATE": None,      # image rotation angle
-                         "EDGE": 0,
-                         "BLUR": None
+        """Reset all settings to default. Activated by key press r or R."""
+        self.SUBMODES = {"TRANSLATE_X": 0,    # pixel offset x direction
+                         "TRANSLATE_Y": 0,    # pixel offset y direction
+                         "FLIP": 1,           # image flip horiz, vert, both
+                         "ROTATE": 0,         # image rotation angle
+                         "EDGE": None,        # change the edge detection mode
+                         "SCALE_X": None,     # adjust x scale of the image
+                         "SCALE_Y": None,     # adjust y scale of the image
+                         "BLUR": None,        # control how much the image is blurred
+                         "COLOR": None,       # adjust the image colors
+                         "SCALE": 1,          # adjust the scale of the image
+                         "FACE": -1,          # adjust minimum neighbors for face detection
+                         "EYES": -1,          # adjust minimum neighbors for eyes detection
+                         "SMILE": -1          # adjust minimum neighbors for smile detection
                          }
-        self.MODE = 0
+        self.COLOR_INDEX = 0
+        self.EDGE_INDEX = 0
 
     def run(self):
-            
+        """Main video loop. Handles key input, modifies frame, and displays frames."""
         capture = cv.VideoCapture(0)
         while True:
             is_running, frame = capture.read()
             key = cv.waitKeyEx(self.FRAME_SPEED)
             if key != -1:
                 print(key)
-            if key == ord('q') or key == ord('Q'):
+            if key in [ord('q'), ord('Q')]:
                 break
             
+            # Reset image adjustments to None
+            elif key in [ord('r'), ord('R')]:
+                self.reset()
+                
+            elif key in [ord('h'), ord('H')]:
+                self.BOX_THICKNESS = -1 if self.BOX_THICKNESS != -1 else 3
+            
             # Change Mode or Submode Values
-            if key in self.ARROW_MAP:
+            elif key in self.ARROW_MAP:
                 self.MESSAGE_TIME = time.time() + self.MESSAGE_DISPLAY_TIME
                 direction = self.ARROW_MAP[key]
                 if direction in ["UP", "DOWN"]:
@@ -125,12 +196,14 @@ class Video:
                 self.update_message(direction)
             
             # Modify frame
+            frame = self.face_detect(frame)
+            frame = self.scale(frame)
             frame = self.adjust_color(frame)
             frame = self.translate(frame)
-            frame = self.rotate(frame)
             frame = self.flip(frame)
             frame = self.blur(frame)
             frame = self.edge_detection(frame)
+            frame = self.rotate(frame)
             
             # Blit message to the user (like current setting or value)
             self.add_message(frame)
@@ -153,7 +226,9 @@ class Video:
         if mode == "FLIP":
             self.SUBMODES[mode] = next(self.FLIP_WHEEL)
         elif mode == "EDGE":
-            self.SUBMODES[mode] = next(self.EDGE_WHEEL)
+            self.EDGE_INDEX += 1 if direction == "UP" else -1
+            self.EDGE_INDEX %= len(self.EDGE_WHEEL)
+            self.SUBMODES[mode] = self.EDGE_WHEEL[self.EDGE_INDEX]
         elif mode == "BLUR":
             if self.SUBMODES[mode] is None:
                 self.SUBMODES[mode] = 1
@@ -171,7 +246,21 @@ class Video:
             self.SUBMODES[mode] += 1 if direction == 'UP' else -1
             self.SUBMODES[mode] %= 360
         elif mode == "COLOR":
-            self.SUBMODES[mode] = next(self.COLOR_WHEEL)
+            self.COLOR_INDEX += 1 if direction == 'UP' else -1
+            self.COLOR_INDEX %= len(self.COLOR_WHEEL)
+            self.SUBMODES[mode] = self.COLOR_WHEEL[self.COLOR_INDEX]
+        elif mode == "SCALE":
+            self.SUBMODES[mode] += 0.1 if direction == 'UP' else -0.1
+            self.SUBMODES[mode] = max(self.SUBMODES[mode], 0.1)
+        elif mode == "FACE":
+            self.SUBMODES[mode] += 1 if direction == 'UP' else -1
+            self.SUBMODES[mode] = max(-1, self.SUBMODES[mode])
+        elif mode == "EYES":
+            self.SUBMODES[mode] += 1 if direction == 'UP' else -1
+            self.SUBMODES[mode] = min(0, max(-1, self.SUBMODES[mode]))
+        elif mode == "SMILE":
+            self.SUBMODES[mode] += 1 if direction == 'UP' else -1
+            self.SUBMODES[mode] = min(0, max(-1, self.SUBMODES[mode]))
             
     def get_mode(self):
         return self.MODE_WHEEL[self.MODE]
@@ -201,11 +290,73 @@ class Video:
                 self.MESSAGE = self.ROTATE_MESSAGES(self.SUBMODES['ROTATE'])
             elif mode == "COLOR":
                 self.MESSAGE = self.COLOR_MESSAGES[self.SUBMODES['COLOR']]
+            elif mode == "SCALE":
+                self.MESSAGE = self.SCALE_MESSAGES(self.SUBMODES['SCALE'])
+            elif mode == "FACE":
+                self.MESSAGE = self.FACE_MESSAGES(self.SUBMODES['FACE'])
+            elif mode == "SMILE":
+                self.MESSAGE = self.SMILE_MESSAGES(self.SUBMODES['SMILE'])
+            elif mode == "EYES":
+                self.MESSAGE = self.EYES_MESSAGES(self.SUBMODES['EYES'])
         
         # MODE CHANGES
         else:
             self.MESSAGE = self.get_mode()
-            
+    
+    # =============================================================================
+    # FACE DETECTION
+    # =============================================================================
+    @staticmethod
+    def _manhattan(x1, y1, x2, y2):
+        return abs(x1 - x2) + abs(y1 - y2)
+    
+    def face_detect(self, frame):
+        min_neighbors = self.SUBMODES['FACE']
+        if min_neighbors == -1:
+            return frame
+        
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        face_rectangles = self.CLF_FACE.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = min_neighbors)
+        if face_rectangles == ():
+            return frame # no faces were found
+        
+        # Draw shape around face
+        x, y, width, height = face_rectangles[0] # for simplicity, just use the first face rectangle for this app
+        center = (x + width // 2, y + height // 2)
+        radius = max(width, height) // 2
+        cv.circle(frame, center, radius, self.BOX_COLOR, thickness = self.BOX_THICKNESS)
+        
+        # Draw shape around eyes
+        if self.SUBMODES['EYES'] != -1:
+            dx, dy = x, y
+            eye_rectangles = self.CLF_EYES.detectMultiScale(gray[x:x+width, y:y+height], scaleFactor = 1.1, minNeighbors = min_neighbors)
+            if eye_rectangles != ():
+                x, y, width, height = eye_rectangles[0]
+                x += dx
+                y += dy
+                center = (x + width // 2, y + height // 2)
+                radius = max(width, height) // 2
+                cv.circle(frame, center, radius, self.BOX_COLOR, thickness = self.BOX_THICKNESS)
+                
+                # Pick second eye that is furthest from the first eye
+                if len(eye_rectangles) > 1:
+                    x, y, width, height = max(eye_rectangles, key = lambda rect: self._manhattan(rect[0], rect[1], x, y)) 
+                    x += dx
+                    y += dy
+                    center = (x + width // 2, y + height // 2)
+                    radius = max(width, height) // 2
+                    cv.circle(frame, center, radius, self.BOX_COLOR, thickness = self.BOX_THICKNESS)
+                    
+        if self.SUBMODES['SMILE'] != -1:
+            smile_rectangle = self.CLF_SMILE.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = min_neighbors)
+            if smile_rectangle != ():
+                x, y, width, height = smile_rectangle[0]
+                center = (x + width // 2, y + height // 2)
+                radius = max(width, height) // 2
+                cv.circle(frame, center, radius, self.BOX_COLOR, thickness = self.BOX_THICKNESS)
+                
+        return frame
+        
     # =============================================================================
     # COLOR OPTIONS
     # =============================================================================
@@ -271,12 +422,31 @@ class Video:
         center = height // 2, width // 2
         rotation_matrix = cv.getRotationMatrix2D(center, angle, 1.0) # point_of_rotation, angle, scale
         return cv.warpAffine(frame, rotation_matrix, (height, width))
+    
+    def scale(self, frame):
+        """Resizes the frame by scale s"""
+        s = self.SUBMODES["SCALE"]
+        if s == 1:
+            return frame
+        return cv.resize(frame, (int(frame.shape[1] * s), int(frame.shape[0] * s)), interpolation=cv.INTER_AREA)
         
     # =============================================================================
     # EDGE AND BLUR FILTERS 
     # =============================================================================
+    def _sobel_x(self, frame, gray = None):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) if gray is None else gray
+        return cv.Sobel(gray, cv.CV_64F, 1, 0) # 1 x dir and 0 y dir
+        
+    def _sobel_y(self, frame, gray = None):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) if gray is None else gray
+        return cv.Sobel(gray, cv.CV_64F, 0, 1) # 0 x dir and 1 y dir
+        
+    def _sobel_xy(self, frame):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        return cv.bitwise_or(self._sobel_x(frame, gray = gray), self._sobel_y(frame, gray = gray))
+    
     def _edge_canny(self, frame):
-        return cv.Canny(frame, 125, 175)
+        return cv.Canny(frame, self.CANNY_THRESHOLD1, self.CANNY_THRESHOLD2)
     
     def _edge_laplacian1(self, frame):
         return cv.Laplacian(frame, cv.CV_64F)
@@ -298,10 +468,9 @@ class Video:
         return cv.blur(frame, (val, val))
 
 if __name__ == "__main__":
-    settings = {
+    video_settings = {
                 'FRAME_SPEED': 50, # one frame per 20 ms
-                'MODE': 0,
-                'SUBMODE': 0
+                'MODE': 0          # Starting edit mode
                 }
     
     text_settings = {
@@ -313,5 +482,18 @@ if __name__ == "__main__":
                 'FONT_POSITION': (10, 30)    
                 }
     
-    video = Video(**settings, **text_settings)
+    classifier_settings = {
+                'CLF_FACE': cv.CascadeClassifier('classifiers/haar_face.xml'),
+                'CLF_EYES': cv.CascadeClassifier('classifiers/haar_eye.xml'),
+                'CLF_SMILE': cv.CascadeClassifier('classifiers/haar_smile.xml'),
+                'BOX_COLOR': (0, 200, 0), # (B, G, R)
+                'BOX_THICKNESS': 3        # set to -1 to fill in the box
+                }
+    
+    edge_settings = {
+                'CANNY_THRESHOLD1': 125,
+                'CANNY_THRESHOLD2': 175
+                }
+    
+    video = Video(**video_settings, **text_settings, **classifier_settings, **edge_settings)
     video.run()
